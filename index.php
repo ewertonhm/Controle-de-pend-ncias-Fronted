aa0e04de1bb8c1a38a592a3f626fb541afcb06a6
Ewerton Henrique Marschalk <you@example.com>
2022-05-17 15:32:45 -0300

    fix login logic

--- index.php
+++ index.php
@@ -8,9 +8,11 @@ if(!isset($_SESSION['logado']) OR $_SESSION['logado'] != true){
     header('location: login.php');
 }else{
     if(!$_SESSION['usuario']->refresh_token()){
-        header('location: login.php');
+        header('location: logout.php');
     }
 }
+
+dump($_SESSION);
 
 $template = $twig->load('index.twig');
 
