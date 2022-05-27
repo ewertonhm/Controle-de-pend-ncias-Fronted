e97043049dd6400e4f711d340338719d4bc43439
ewerton.marschalk <ewerton.marschalk@redeunifique.com.br>
2022-05-27 13:29:49 -0300

    invert sorting order

--- classes/Controllers/Sort.php
+++ classes/Controllers/Sort.php
@@ -4,14 +4,19 @@ namespace Controller;
 
 class Sort
 {
-    public function date_sorting_inicio($a, $b)
+    public function date_sorting_inicio_fim($a, $b)
     {
         return strtotime($b->inicio) - strtotime($a->inicio);
     }
 
+    public function date_sorting_fim_inicio($a, $b)
+    {
+        return strtotime($a->inicio) - strtotime($b->inicio);
+    }
+
     public function sort_by_inicio($pendencias)
     {
-        usort($pendencias, array($this, 'date_sorting_inicio'));
+        usort($pendencias, array($this, 'date_sorting_fim_inicio'));
         return $pendencias;
     }
 }
