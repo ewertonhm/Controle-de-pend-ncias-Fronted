1bf6f93c75149d267c5cac9a378861fecd706727
ewerton.marschalk <ewerton.marschalk@redeunifique.com.br>
2022-05-27 10:27:49 -0300

    fix issue when there is no pendencias

--- index.php
+++ index.php
@@ -41,72 +41,77 @@ if (!isset($_SESSION['logado']) or $_SESSION['logado'] != true) {
         // if no post:
         // PENDENCIAS
         $raw_pendencias = $pendencias->get_all();
-        // sort
-        $raw_pendencias = $sorting->sort_by_inicio($raw_pendencias);
+
+        if (count($raw_pendencias) > 0) {
+            // sort
+            $raw_pendencias = $sorting->sort_by_inicio($raw_pendencias);
 
 
-        $p = [];
-        // TODO: filtrar direto no backend futuramente
-        // Quando tiver muitas entradas no banco essa etapa pode começar a ficar muito lenta.
-        if (isset($_GET['concluidos'])) {
-            $vars['concluidos'] = 'active';
-            foreach ($raw_pendencias as $pendencia) {
+            $p = [];
+            // TODO: filtrar direto no backend futuramente
+            // Quando tiver muitas entradas no banco essa etapa pode começar a ficar muito lenta.
+            if (isset($_GET['concluidos'])) {
+                $vars['concluidos'] = 'active';
+                foreach ($raw_pendencias as $pendencia) {
+                    if ($pendencia->fim != null) {
+                        $p[] = $pendencia;
+                    }
+                }
+            } elseif (isset($_GET['all'])) {
+                $vars['all'] = 'active';
+                $p = $raw_pendencias;
+            } else {
+                $vars['index'] = 'active';
+                foreach ($raw_pendencias as $pendencia) {
+                    if ($pendencia->fim == null) {
+                        $p[] = $pendencia;
+                    }
+                }
+            }
+
+            // Add pagination / create array_chunks
+            $pp = array_chunk($p, 20, false);
+
+            $vars['pages'] = count($pp);
+
+            if (isset($_GET['page'])) {
+                $p = $pp[(int)$_GET['page']];
+                $vars['page'] = (int) $_GET['page'];
+            } else {
+                $p = $pp[0];
+                $vars['page'] = 0;
+            }
+
+            foreach ($p as $pendencia) {
+                $pendencia->{'hora_abertura'} = Controller\Date::convertFromJsToHumanPluOne($pendencia->created_at);
+
+                if ($pendencia->inicio != null) {
+                    $pendencia->inicio = Controller\Date::convertFromJsToHuman($pendencia->inicio);
+                }
                 if ($pendencia->fim != null) {
-                    $p[] = $pendencia;
+                    $pendencia->fim = Controller\Date::convertFromJsToHuman($pendencia->fim);
+                    $pendencia->{'hora_fechamento'} = Controller\Date::convertFromJsToHumanPluOne($pendencia->updated_at);
+                }
+                if ($pendencia->previsao != null) {
+                    $isAtrasado = Controller\Date::checkIfIsAtrasado($pendencia->previsao);
+                    if ($isAtrasado) {
+                        $pendencia->{'atrasado'} = true;
+                    }
+                    $pendencia->previsao = Controller\Date::convertFromJsToHuman($pendencia->previsao);
+                    $andamentos_all = $andamentos->get_all($pendencia->id);
+
+                    foreach ($andamentos_all as $andamento) {
+                        $andamento->{'hora'} = Controller\Date::convertFromJsToHumanPluOne($andamento->created_at);
+                        $pendencia->{'andamentos'}[] = $andamento;
+                    }
                 }
             }
-        } elseif (isset($_GET['all'])) {
-            $vars['all'] = 'active';
-            $p = $raw_pendencias;
-        } else {
-            $vars['index'] = 'active';
-            foreach ($raw_pendencias as $pendencia) {
-                if ($pendencia->fim == null) {
-                    $p[] = $pendencia;
-                }
-            }
+            $tipos = new \Controller\TipoPendencia($_SESSION['usuario']->token);
+
+            $vars['pendencias'] = $p;
         }
 
-        // Add pagination / create array_chunks
-        $pp = array_chunk($p, 20, false);
 
-        $vars['pages'] = count($pp);
-
-        if (isset($_GET['page'])) {
-            $p = $pp[(int)$_GET['page']];
-            $vars['page'] = (int) $_GET['page'];
-        } else {
-            $p = $pp[0];
-            $vars['page'] = 0;
-        }
-
-        foreach ($p as $pendencia) {
-            $pendencia->{'hora_abertura'} = Controller\Date::convertFromJsToHumanPluOne($pendencia->created_at);
-
-            if ($pendencia->inicio != null) {
-                $pendencia->inicio = Controller\Date::convertFromJsToHuman($pendencia->inicio);
-            }
-            if ($pendencia->fim != null) {
-                $pendencia->fim = Controller\Date::convertFromJsToHuman($pendencia->fim);
-                $pendencia->{'hora_fechamento'} = Controller\Date::convertFromJsToHumanPluOne($pendencia->updated_at);
-            }
-            if ($pendencia->previsao != null) {
-                $isAtrasado = Controller\Date::checkIfIsAtrasado($pendencia->previsao);
-                if ($isAtrasado) {
-                    $pendencia->{'atrasado'} = true;
-                }
-                $pendencia->previsao = Controller\Date::convertFromJsToHuman($pendencia->previsao);
-                $andamentos_all = $andamentos->get_all($pendencia->id);
-
-                foreach ($andamentos_all as $andamento) {
-                    $andamento->{'hora'} = Controller\Date::convertFromJsToHumanPluOne($andamento->created_at);
-                    $pendencia->{'andamentos'}[] = $andamento;
-                }
-            }
-        }
-        $tipos = new \Controller\TipoPendencia($_SESSION['usuario']->token);
-
-        $vars['pendencias'] = $p;
 
         $vars['tipos'] = $tipos->get_all();
 
